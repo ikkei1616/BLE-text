4d6ad256b7626dc760b80f9b406a7a1064a6ff65
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2024-12-13 20:02:54 +0900

    Initial Commit

---
+++ CLE/CLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLEApp.swift
+//  CLE
+//
+//  Created by 神宮一敬 on 2024/12/13.
+//
+
+import SwiftUI
+
+@main
+struct CLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CLE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CLE
+//
+//  Created by 神宮一敬 on 2024/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
